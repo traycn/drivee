0acade44350bbf86e71018cbbcc1dd9283a0487a
TN <16641980+traycn@users.noreply.github.com>
2023-11-07 15:58:26 -0500

    Fix overlay.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,14 +28,9 @@ const Hero = () => {
 
       <div className='hero__image-container'>
         <div className='hero__image'>
-          <Image 
-            src="/hero.png" 
-            alt="hero"
-            fill
-            className='object-contain'
-          />
-          <div className='hero__image-overlay'/>
+          <Image src="/hero.png" alt="hero" fill className='object-contain'/>
         </div>
+        <div className='hero__image-overlay'/>
       </div>
     </div>
   )
