7b1678ba0dda44d72894a236dc8756777bf8d652
TN <16641980+traycn@users.noreply.github.com>
2023-11-07 22:41:22 -0500

    Initial car catalog feature.

---
+++ components/shared/CustomFilter.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { CustomFilterProps } from '@/types';
+
+const CustomFilter = ({
+  title
+} : CustomFilterProps) => {
+
+  
+  return (
+    <div>CustomeFilter</div>
+  )
+}
+
+export default CustomFilter
